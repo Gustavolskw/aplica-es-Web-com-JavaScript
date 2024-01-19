4a76194c2bd48cc60d67ad05560af45f91a0c575
gustavo.luis <gustavolschmidt13@gmail.com>
2024-01-19 17:53:56 -0300

    final project of JsVidFlow

--- projeto-vidflow/script.js
+++ projeto-vidflow/script.js
@@ -13,11 +13,14 @@ let timerInSeconds = 5;
 
     async function buscarEMostrarVideos() {
         try {
-            const busca = await fetch("http://localhost:3000/video");
+            const busca = await fetch("http://localhost:3000/videos");
         const videos = await busca.json();
             videos.forEach(video => {
+                if (video.categoria == "") {
+                    throw new Error("Categorias nao encontradas!!!!")
+                }
                 containerVideos.innerHTML += `
-                <li class=" border border-1 border-secondary rounded-2 mb-4 px-0 col-lg-2 col-sm-12 col-md-5 mx-4">
+                <li class="videos__item border border-1 border-secondary rounded-2 mb-4 px-0 col-lg-3 col-sm-12 col-md-5 mx-4 ">
                 <div>
                 <iframe class="border border-1" src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                     <div class="descricao-video px-1 ">
@@ -26,6 +29,7 @@ let timerInSeconds = 5;
                             <h3 class="titulo-video text-start overflow-auto text-wrap">${video.titulo}</h3>
                         </div>
                         <p class="titulo-canal text-start postion-absolute bottom-0 pb-2 ps-1">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </div>                                                  
                 </li>`
@@ -72,15 +76,43 @@ timerInSeconds -= 1;
            
            
             
-            }
+        }/* finally {
+            alert("conexçao bem sucedida")
+            
+        }
+        */
     }
 buscarEMostrarVideos();
 
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+barraDePesquisa.addEventListener("input", filtrarPesquisa)
 
-   
-  
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll('.videos__item');
+  const valorFiltro = barraDePesquisa.value.toLowerCase();
 
+  videos.forEach((video) => {
+    const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
 
-
-
-
+    video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+  });
+}
+const botaoCategoria = document.querySelectorAll(".superior__item")
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => 
+        filtrarPorCategoria(nomeCategoria))
+})
+ 
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item")
+    for (let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+        if (!categoria.includes(valorFiltro)&&valorFiltro!="tudo") {
+            video.style.display = "none"
+        } else {
+            video.style.display = "block"
+        }
+    }
+}
